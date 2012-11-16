751a797a419c2c3f40577ff4245944df525d0e33
Matt Waite <matt.waite@gmail.com>
2012-11-15 22:32:36 -0600

    Collection created, data displaying.

--- MeteorMonitor.js
+++ MeteorMonitor.js
@@ -1,9 +1,15 @@
+// Set up a collection to contain player information. On the server,
+// it is backed by a MongoDB collection named "players".
+
+Pulses = new Meteor.Collection("pulses");
+
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to MeteorMonitor.";
+  Template.monitor.pulses = function () {
+    return Pulses.find({}, {sort: {timestamp: -1}});
   };
 
-  Template.hello.events({
+
+  Template.monitor.events({
     'click input' : function () {
       // template data, if any, is available in 'this'
       if (typeof console !== 'undefined')
